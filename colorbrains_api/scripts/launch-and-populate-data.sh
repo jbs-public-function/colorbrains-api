5f05cd5f6b59acd294a6db5b44200ee251b4225f
James Barrett <jbs.public.function@gmail.com>
2021-08-20 01:28:31 -0500

    Modify Launch Script To Fail If Any Error (set -x). Refactor Docker-Compose .yml To Connect To Postgres Service. Error Handling If Duplicates Exist.

--- colorbrains_api/scripts/launch-and-populate-data.sh
+++ colorbrains_api/scripts/launch-and-populate-data.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-python3 /colorbrains_api/scripts/populate_database.py
+set -e
+
+python3 ./colorbrains_api/scripts/populate_database.py
 uvicorn main:app --host 0.0.0.0 --port 80
--- colorbrains_api/scripts/populate_database.py
+++ colorbrains_api/scripts/populate_database.py
@@ -2,5 +2,18 @@
 @todo - fill out populate data scripts
 """
 
-print('hello world')
 from colorbrains_api.db_assistant import DbAssistant
+
+import psycopg2
+
+# test code
+# todo - replace with meaningfull populate data code
+
+sql = ''' insert into matplotlib.basecolors(color_name, red, green, blue) VALUES ('xcolor', .35, .45, .55)'''
+try:
+    with DbAssistant().connection as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql)
+        conn.commit()
+except psycopg2.errors.UniqueViolation as err:
+    print(err)
