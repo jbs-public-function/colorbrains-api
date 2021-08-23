0b562c5c1d05fb1c990d4deccd9d7699808299ed
James Barrett <jbs.public.function@gmail.com>
2021-08-22 22:56:11 -0500

    Add Endpoints For Initial Color Tables Created. /basecolors, /categorized_colormaps, /colormaps, /namedcolors. These Are Simple Get Operations That Will Dump All Data. No Filtering Necessary At This Stage Of Development

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-from typing import Optional
-
 from fastapi import FastAPI
 
-from colorbrains_api.db_assistant import DbAssistant
+from colorbrains_api import get_sql
 
 
 app = FastAPI()
 
-"""
-@todo - flesh out endpoints
-"""
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/basecolors")
+def basecolors():
+    return get_sql.get_basecolors()
 
 
-@app.get("/read")
-def read_root2():
-    return {"Hello": "World"}
+@app.get("/categorized_colormaps")
+def categorized_colormaps():
+    return get_sql.get_categorized_colormaps()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/colormaps")
+def colormaps():
+    return get_sql.get_colormaps()
+
+
+@app.get("/namedcolors")
+def namedcolors():
+    return get_sql.get_namedcolors()
